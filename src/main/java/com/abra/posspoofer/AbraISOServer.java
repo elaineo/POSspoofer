95a8f48fe24aef17cc9c9bc885f2f66fcba5169d
Elaine Ou <elaine.ou@gmail.com>
2016-02-17 18:48:11 -0800

    update templates

--- src/main/java/com/abra/posspoofer/AbraISOServer.java
+++ src/main/java/com/abra/posspoofer/AbraISOServer.java
@@ -17,9 +17,11 @@ import org.jpos.util.SimpleLogListener;
 public class AbraISOServer implements ISORequestListener { 
 	public static final String UB_SERVER = "202.14.87.10";
 	public static final Integer UB_PORT = 8012;
+
 	
     public boolean process (ISOSource source, ISOMsg m) { 
         try { 
+        	System.out.println("something received");
             m = (ISOMsg) m.clone (); 
             m.setResponseMTI(); 
             m.set (new ISOField(39, "99")); 
@@ -33,21 +35,59 @@ public class AbraISOServer implements ISORequestListener {
     public static void main(String args[]) throws ISOException { 
     	
     	GenericPackager packager = new GenericPackager("ub_request.xml");
-
-    	byte b[] = new byte[5]; 
-    	byte[] isoHeader = ISOUtil.hex2byte("6001208100");
     
+    	// (Unionbank Debit Message specs)
+		ISOMsg isoMsg = new ISOMsg();
+		isoMsg.setPackager(packager);
+		isoMsg.setMTI("0200");
+		isoMsg.set(2,"4404520100000034");
+		isoMsg.set(3,"001000");
+		isoMsg.set(4, "10000");
+		isoMsg.set(7, "0217221800");
+		isoMsg.set(11, "123456");
+		isoMsg.set(12, "221800");
+		isoMsg.set(13, "0216");
+		isoMsg.set(22,"021");
+		isoMsg.set(24,"827");
+		isoMsg.set(25,"00");
+		//isoMsg.set(35, "0");
+		
+		isoMsg.set(41, "ELAINEOU");
+		isoMsg.set(42, "ABRAGLOBAL01234");
+		
+		
+		
+		/*
+		byte[] pin = "374181".getBytes();
+		BitSet pinb = ISOUtil.int2BitSet(374181, 0);
+		isoMsg.set("52", ISOUtil.bitSet2byte(pinb));
+ 		*/	
+		// print the DE list
+		//logISOMsg(isoMsg);
+ 
+		// Get and print the output result
+		byte[] data = isoMsg.pack();
+		System.out.println("RESULT : " + new String(data));
+		
+		// Create TPDU header
+		byte[] isoHeader = ISOUtil.hex2byte("6001208100");
+    	
         try { 
             Logger logger = new Logger(); 
             logger.addListener (new SimpleLogListener (System.out)); 
 
-            ISOChannel clientSideChannel = new NACChannel(UB_SERVER, UB_PORT, packager, b);
-
+            NACChannel clientSideChannel = new NACChannel(UB_SERVER, UB_PORT, packager, isoHeader);
+            clientSideChannel.setLogger(logger, null); 
+            
             ISOServer server = new ISOServer(UB_PORT, (ServerChannel) clientSideChannel, null);
 
             server.setLogger (logger, "iso-server"); 
             server.addISORequestListener (new AbraISOServer()); 
-            new Thread (server).start(); 
+            
+            new Thread (server).start();
+            
+            clientSideChannel.connect();
+            clientSideChannel.send(data);
         } catch (Exception e) { 
             e.printStackTrace(); 
         } 
--- src/main/java/com/abra/posspoofer/BuildISOMessage.java
+++ src/main/java/com/abra/posspoofer/BuildISOMessage.java
@@ -1,6 +1,10 @@
 package com.abra.posspoofer;
  
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
+
 import org.jpos.iso.ISOException;
 import org.jpos.iso.ISOMsg;
 import org.jpos.iso.ISOUtil;
@@ -18,25 +22,61 @@ public class BuildISOMessage {
 		// Create logger to dump channel output
 		Logger logger = new Logger();
 		logger.addListener(new SimpleLogListener (System.out)); 
+		
+		HashMap<String, String> transaction = new HashMap<String, String>();
+		transaction.put("2","4404520100000034");
+		transaction.put("3","001000");
+		transaction.put("22","021");
+		transaction.put("24","827");
+		transaction.put("25","00");
+		transaction.put("35","0"); // FIXME
+		
+		
+		Set set = transaction.entrySet();
+	    Iterator iterator = set.iterator();
  
 		// Create ISO Message 
 		// (Unionbank Debit Message specs)
 		ISOMsg isoMsg = new ISOMsg();
 		isoMsg.setPackager(packager);
 		isoMsg.setMTI("0200");
-		isoMsg.set(2, "4404520100000034");
-		isoMsg.set(3, "001000");
+		/*
+    	while(iterator.hasNext()) {
+    		Map.Entry mentry = (Map.Entry)iterator.next();
+    		byte [] bval = ISOUtil.str2bcd("4404520100000034", false);
+    		System.out.println(bval);
+    		isoMsg.set((String) mentry.getKey(), bval);
+    	}
+    	*/
+		
+		//isoMsg.set(4, "10000");
+		//isoMsg.set(7, "0217221800");
+		//isoMsg.set(11, "123456");
+		//isoMsg.set(12, "221800");
+		//isoMsg.set(13, "0216");
+    	
+		isoMsg.set(2,"4404520100000034");
+		isoMsg.set(3,"001000");
 		isoMsg.set(4, "10000");
-		isoMsg.set(7, "0120221800");
+		isoMsg.set(7, "0217221800");
 		isoMsg.set(11, "123456");
 		isoMsg.set(12, "221800");
-		isoMsg.set(22, "021");
-		isoMsg.set(24, "827");
-		isoMsg.set(25, "00");
-		//isoMsg.set(35, "");
+		isoMsg.set(13, "0216");
+		isoMsg.set(22,"021");
+		isoMsg.set(24,"827");
+		isoMsg.set(25,"00");
+		//isoMsg.set(35, "0");
+		
 		isoMsg.set(41, "ELAINEOU");
 		isoMsg.set(42, "ABRAGLOBAL01234");
- 
+		
+		
+		//EncryptedPIN pin = BaseSMAdapter.encryptPIN("blah","blah", true);
+		/*
+		byte[] pin = "374181".getBytes();
+		BitSet pinb = ISOUtil.int2BitSet(374181, 0);
+		isoMsg.set("52", ISOUtil.bitSet2byte(pinb));
+ 		*/	
 		// print the DE list
 		logISOMsg(isoMsg);
  
@@ -48,14 +88,14 @@ public class BuildISOMessage {
 		byte[] isoHeader = ISOUtil.hex2byte("6001208100");
 		
 		// Send to endpoint
-		// test: 203.131.75.230:8012
 		NACChannel nc = new NACChannel("202.14.87.10", 8012, packager, isoHeader);
 		nc.setLogger(logger, null); 
 		nc.connect();
 		nc.send(isoMsg);
 		ISOMsg r = nc.receive();
 		logISOMsg(r);
-		nc.send(data);
+		//nc.send(data);
+		
 	}
  
 	private static void logISOMsg(ISOMsg msg) {
